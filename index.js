484a6125c4b20d3adcaae1ad00a9464d8743f454
Rolando Santamaria Maso <rolando.santamaria_maso@daimler.com>
2018-05-29 10:58:08 +0200

    minification

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-function rstringify (o) {
-  const type = getType(o)
+function s (o) {
+  const t = getType(o)
 
-  if (type === 'string' || o instanceof Date) return `"${o}"`
-  if (type === 'object') {
+  if (t === 'string' || o instanceof Date) return `"${o}"`
+  if (t === 'object') {
     const keys = Object.keys(o)
     if (!keys.length) return '{}'
-    return keys.reduce((acc, k, index, arr) => `${acc}"${k}":${rstringify(o[k])}${index + 1 < arr.length ? ',' : '}'}`, '{')
+    return keys.reduce((acc, k, i, arr) => `${acc}"${k}":${s(o[k])}${i + 1 < arr.length ? ',' : '}'}`, '{')
   }
-  if (type === 'array') {
+  if (t === 'array') {
     if (!o.length) return '[]'
-    return o.reduce((acc, e, index, arr) => `${acc}${rstringify(e)}${index + 1 < arr.length ? ',' : ']'}`, '[')
+    return o.reduce((acc, e, i, arr) => `${acc}${s(e)}${i + 1 < arr.length ? ',' : ']'}`, '[')
   }
 
   return `${o}`
 }
 
 function getType (o) {
-  const type = typeof o
-  if (type === 'object') {
+  const t = typeof o
+  if (t === 'object') {
     if (o === null) return 'null'
     if (undefined === o) return 'undefined'
     if (Array.isArray(o)) return 'array'
   }
 
-  return type
+  return t
 }
 
 module.exports = {
   stringify: (o) => {
-    const type = getType(o)
-    if (o === null || o === undefined || type !== 'object') return `"${o}"`
+    const t = getType(o)
+    if (o === null || o === undefined || t !== 'object') return `"${o}"`
 
-    return rstringify(o)
+    return s(o)
   }
 }
